e687514caa664957d7276954a4fbe9fbdff19694
mingyo <wjdalsry_jmg@naver.com>
2023-03-23 15:22:33 +0900

    [DEL] 오류난 파일들 삭제

--- noti-model/src/main/java/com/vazil/noti/slack/composition/Dispatch.java
+++
@@ -1,9 +0,0 @@
-package com.vazil.noti.slack.composition;
-
-import lombok.Builder;
-
-@Builder
-public class Dispatch {
-    //https://api.slack.com/reference/block-kit/composition-objects#dispatch_action_config
-    private String[] trigger_actions_on;
-}
