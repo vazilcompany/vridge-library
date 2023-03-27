3bc1e880b3640e776c4fab6dbca4390d682003af
mingyo <wjdalsry_jmg@naver.com>
2023-03-27 09:01:53 +0900

    [ADD] 배포 시 소스 파일 추가

--- noti-backend/src/main/java/com/vazil/noti/aspect/NotificationAspect.java
+++ noti-backend/src/main/java/com/vazil/noti/aspect/NotificationAspect.java
@@ -1,21 +1,25 @@
 package com.vazil.noti.aspect;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 
 @Aspect
 @Log4j2
-@Component
+@AutoConfigurationPackage
+@AllArgsConstructor
 public class NotificationAspect {
 
     @Around("@annotation(com.vazil.noti.annotation.Notification)")
     public Object NotificationAround(ProceedingJoinPoint pjp) throws Throwable {
-        Object proceed = pjp.proceed();
 
+
+        Object proceed = pjp.proceed();
         log.info("안녕하세요 aspect가 되는지 확인할려고 합니다.");
+
 
         return proceed;
     }
